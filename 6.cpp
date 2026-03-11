04759e546648453edceed6375a6739909596231e
CdatarET <datar.chaitanya@gmail.com>
2026-03-11 12:39:05 +0000

    copy to RAM

--- 6.cpp
+++ 6.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main(){
-    cout << "Hello World" << endl;
+class Student{
+    private:
+        int prn;
+        string name;
+        char panel;
 
-    return(0);
+    public:
+        Student() {}
+
+        Student(int prn, string name, char panel){
+            this->prn = prn;
+            this->name = name;
+            this->panel = panel;
+        }
+
+        void disp(){
+            cout << "PRN: " << prn << endl;
+            cout << "Name: " << name << endl;
+            cout << "Panel: " << panel << endl << endl;
+        }
+
+        void write(ofstream &file2){
+            file2 << prn << " " << name << " " << panel << endl;
+        }
+};
+
+int main(){
+
+    Student s[10];
+    int c = 0;
+
+    ifstream file1("file1.txt");
+
+    int prn;
+    string name;
+    char panel;
+    while(file1 >> prn >> name >> panel){
+        s[c++] = Student(prn, name, panel);
+    }
+    file1.close();
+
+    cout << "Data Recorded\n\n";
+
+    for(int i = 0; i < c; i++){
+        s[i].disp();
+    }
+
+    return 0;
 }
